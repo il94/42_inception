9088b5b032549860e30a8acf9b797db41bd65c79
il94 <ilyes.landolsi@outlook.fr>
2023-07-26 21:20:52 +0200

    tmp

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,22 +1,29 @@
 sleep 10
 
-if ! test -f "/var/www/wordpress/wp-config.php"; then
-	wp config create	--allow-root \
+if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
+	wp --path='/var/www/wordpress' config create \
+						--allow-root \
 						--dbname=$SQL_DATABASE \
 						--dbuser=$SQL_USER \
 						--dbpass=$SQL_PASSWORD \
-						--dbhost=mariadb:3306 --path='/var/www/wordpress'
-	wp core install		--allow-root \
-						--url=http://ilandols.42.fr \
-						--title="Mon Site" \
-						--admin_user=Add_mine \
-						--admin_password=azerty \
-						--admin_email=admin@example.com
+						--dbhost=mariadb:3306
+						
+	# wp --path='/var/www/wordpress' core install \
+	# 					--allow-root \
+	# 					--url=http://ilandols.42.fr \
+	# 					--title="Mon Site" \
+	# 					--admin_user=Add_mine \
+	# 					--admin_password=azerty \
+	# 					--admin_email=admin@example.com
 
-	wp user create		--allow-root \
-						ilandols \
-						ilyes@mail.fr \
-						--user_pass=azerty \
-						--display_name="Ilyes Landolsi"
-
+	# wp --path='/var/www/wordpress' user create
+	# 					--allow-root \
+	# 					ilandols \
+	# 					ilyes@mail.fr \
+	# 					--user_pass=azerty \
+	# 					--display_name="Ilyes Landolsi"
+else
+	echo "CA EXISTE"
 fi
+
+/usr/sbin/php-fpm7.3 -F
